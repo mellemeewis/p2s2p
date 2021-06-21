a08ee0d41b33f2da2e18ae1fd03ba882c21ab8b2
Melle meewis <mellemeewi@icloud.com>
2021-06-21 12:19:30 +0200

    minor

--- options/train_options.py
+++ options/train_options.py
@@ -28,6 +28,7 @@ class TrainOptions:
 		self.parser.add_argument('--learn_in_w', action='store_true', help='Whether to learn in w space instead of w+')
 
 		self.parser.add_argument('--lpips_lambda', default=0.8, type=float, help='LPIPS loss multiplier factor')
+		self.parser.add_argument('--kl_lambda', default=1, type=float, help='KL loss multiplier factor')
 		self.parser.add_argument('--id_lambda', default=0, type=float, help='ID loss multiplier factor')
 		self.parser.add_argument('--l2_lambda', default=1.0, type=float, help='L2 loss multiplier factor')
 		self.parser.add_argument('--w_norm_lambda', default=0, type=float, help='W-norm loss multiplier factor')
