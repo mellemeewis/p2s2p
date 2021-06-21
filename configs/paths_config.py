5a55ea083dd0ffe50f478ee12db786f39724d97e
Melle meewis <mellemeewi@icloud.com>
2021-06-21 11:28:41 +0200

    minor

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -8,13 +8,25 @@ dataset_paths = {
 	'ffhq': '',
 }
 
+# model_paths = {
+# 	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
+# 	'ir_se50': 'pretrained_models/model_ir_se50.pth',
+# 	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
+# 	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
+# 	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
+# 	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+# 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
+# 	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar'
+# }
+
 model_paths = {
-	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
-	'ir_se50': 'pretrained_models/model_ir_se50.pth',
-	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
-	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
-	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
-	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+	'stylegan_ffhq': '/content/drive/MyDrive/pretrained_models/stylegan2-ffhq-config-f.pt',
+	'ir_se50': '/content/drive/MyDrive/pretrained_models/model_ir_se50.pth',
+	'circular_face': '/content/drive/MyDrive/pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': '/content/drive/MyDrive/pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': '/content/drive/MyDrive/pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': '/content/drive/MyDrive/pretrained_models/mtcnn/onet.npy',
 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
-	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar'
+	'moco': '/content/drive/MyDrive/pretrained_models/moco_v2_800ep_pretrain.pth.tar'
 }
+
